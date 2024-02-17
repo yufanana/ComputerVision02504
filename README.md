3f38ccc2ad1104e124d0b208d93809c9fd3e8437
yufanana <yufan.fong@gmail.com>
2024-02-17 21:45:54 +0100

    Added quiz 3

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 This repository contains materials used as part of the course 02504 Computer Vision, at DTU in Fall 2024.
 
 Please submit a pull request if you find any mistakes or you would like to contribute.
+
+## Topics Covered
+
+- Week 1: Homogenous coordinates, pinhole model, projection
+- Week 2: Camera model, lens distortion, homography, point normalization
+- Week 3: Multiview geometry, epipolar, triangulation
--- exercises/util_functions.py
+++ exercises/util_functions.py
@@ -1,9 +1,26 @@
 """
-Utilty functions commonly used in exercises.
+Utilty functions commonly used in exercises and quizzes.
 """
 import numpy as np
 import itertools as it
+import matplotlib.pyplot as plt
 
+
+# Ex 1.7
+def point_line_distance(line, p):
+    """
+    Calculate shortest distance d between line l and 2D homogenous point p.
+
+    line : 3x1 vector
+    p : 3x1 vector
+    """
+    if p.shape != (3,1):
+        raise ValueError("p must be a 3x1 homogenous vector")
+    if line.shape != (3,1):
+        raise ValueError("line must be a 3x1 homogenous vector")
+
+    d = abs(line.T @ p) / (abs(p[2]) * np.sqrt(line[0] ** 2 + line[1] ** 2))
+    return d
 
 # Ex 1.12
 def Pi(ph):
@@ -36,23 +53,25 @@ def box3d(n=16):
     return np.hstack(points) / 2
 
 
-# Ex 1.13
-# def projectpoints(K, R, t, Q):
-#     """
-#     Project 3D points in Q onto a 2D plane of a camera
+def projection_matrix(K, R, t):
+    """
+    Create a projection matrix from camera parameters.
 
-#     K : 3 x 3, intrinsic camera matrix
-#     R : 3 x 3, rotation matrix
-#     t: 3 x 1, translation matrix
-#     Q: 3 x n, 3D points matrix
+    K : 3x3 np.array, intrinsic camera matrix
+    R : 3x3 np.array, rotation matrix
+    t : 3x1 np.array, translation matrix
 
-#     P : 2 x n, 2D points matrix
-#     """
-#     Qh = PiInv(Q)  # 4 x n
-#     pose = np.hstack((R, t))  # 3 x 4
-#     Ph = K @ pose @ Qh  # 3 x n
-#     P = Pi(Ph)  # 2 x n
-#     return P
+    P : 3x4 np.array, projection matrix
+    """
+    if K.shape != (3, 3):
+        raise ValueError("K must be a 3x3 matrix")
+    if R.shape != (3, 3):
+        raise ValueError("R must be a 3x3 matrix")
+    if t.shape != (3, 1):
+        raise ValueError("t must be a 3x1 matrix")
+
+    P = K @ np.hstack((R, t))
+    return P
 
 
 # Ex 2.2
@@ -112,3 +131,94 @@ def distort(q, distCoeffs):
         correction += distCoeffs[i] * r**exp
     qd = q * correction
     return qd
+
+
+# Ex 3.2
+def CrossOp(r):
+    '''Returns the cross product operator of r.'''
+    if r.shape == (3,1):
+        r = r.flatten()
+    if r.shape != (3,):
+        raise ValueError('r must be a 3x1 vector')
+
+    return np.array([
+        [0,-r[2],r[1]],
+        [r[2],0,-r[0]],
+        [-r[1],r[0],0],
+    ])
+
+
+# Ex 3.3
+def essential_matrix(R, t):
+    '''Returns the essential matrix.'''
+    return CrossOp(t) @ R
+
+def fundamental_matrix(K1, R1, t1, K2, R2, t2):
+    '''
+    Returns the fundamental matrix, assuming camera 1 coordinates are
+    on top of global coordinates.
+    '''
+    if R1.shape != (3,3) or R2.shape != (3,3):
+        raise ValueError('R1 and R2 must be 3x3 matrices')
+    if t1.shape == (3,) or t2.shape == (3,):
+        t1 = t1.reshape(-1,1)
+        t2 = t2.reshape(-1,1)
+    if t1.shape != (3,1) or t2.shape != (3,1):
+        raise ValueError('t1 and t2 must be 3x1 matrices')
+    if K1.shape != (3,3) or K2.shape != (3,3):
+        raise ValueError('K1 and K2 must be 3x3 matrices')
+
+    # When the {camera1} and {camera2} are not aligned with {global}
+    R_tilde = R2 @ R1.T
+    t_tilde = t2 - R_tilde @ t1
+
+    E = essential_matrix(R_tilde, t_tilde)
+    F = np.linalg.inv(K2).T @ E @ np.linalg.inv(K1)
+    return F
+
+
+# Ex 3.9
+def DrawLine(l, shape):
+    # Checks where the line intersects the four sides of the image
+    # and finds the two intersections that are within the frame
+
+    def in_frame(l_im):
+        '''Returns the intersection point of the line with the image frame.'''
+        q = np.cross(l.flatten(), l_im) # intersection point
+        q = q[:2]/q[2]                  # convert to inhomogeneous
+        if all(q>=0) and all(q+1<=shape[1::-1]):
+            return q
+
+    # 4 edge lines of the image
+    lines = [
+        [1, 0, 0],             # x = 0
+        [0, 1, 0],             # y = 0
+        [1, 0, 1-shape[1]],    # x = shape[1]
+        [0, 1, 1-shape[0]],
+    ]    # y = shape[0]
+
+    P = [in_frame(l_im) for l_im in lines if in_frame(l_im) is not None]
+
+    if (len(P)==0):
+        print("Line is completely outside image")
+    plt.plot(*np.array(P).T)
+
+
+# Ex 3.11
+def triangulate(q_list,P_list):
+    '''
+    Triangulate a single 3D point seen by n cameras.
+
+    q_list : nx2x1 list of 2x1 pixel points
+    P_list : nx3x4, list of 3x4 camera projection matrices
+    '''
+    B = []  # 2n x 4 matrix
+    for i in range(len(P_list)):
+        qi = q_list[i]
+        P_i = P_list[i]
+        B.append(P_i[2]*qi[0]-P_i[0])
+        B.append(P_i[2]*qi[1]-P_i[1])
+    B = np.array(B)
+    U, S, Vt = np.linalg.svd(B)
+    Q = Vt[-1,:-1]/Vt[-1,-1]
+    return Q
